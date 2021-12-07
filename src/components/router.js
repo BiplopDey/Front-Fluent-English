578ecc67a8347bba108db87a6d01ea48d6a9fa9a
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-12-07 11:45:53 +0100

    add routes

--- src/components/router.js
+++ src/components/router.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
 import Ejemplo from "./ejemplo";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Route path="/" element={<App />} />
-      <Route path="/ejemplo" element={<Ejemplo />} />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ejemplo" element={<Ejemplo />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Router from "./components/router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
