947c3e1e2b8b20199a40d872f3145e41613aa32e
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-01-10 17:58:35 +0100

    Add wordsList object

--- src/services/WordsList.js
+++ src/services/WordsList.js
@@ -1,32 +1,30 @@
-export class WordsList {
-  constructor(list) {
-    this.list = list;
-  }
+export const wordsList = {
+  list: [],
   addAll(list) {
     this.list = [...list];
-  }
+  },
   add(word) {
     this.list = [word, ...this.list];
-  }
+  },
   delete(word) {
     const index = this.findIndexById(word.id);
     this.list.splice(index, 1);
-  }
+  },
 
   update(word) {
     const index = this.findIndexById(word.id);
     this.list[index] = word;
-  }
+  },
 
   findIndexById(id) {
     this.list.findIndex((word) => word.id == id);
-  }
+  },
 
   isEmpty() {
     return this.list.length === 0;
-  }
+  },
 
   startsWith(str) {
     return this.list.filter((word) => word.name.startsWith(str));
-  }
-}
+  },
+};
