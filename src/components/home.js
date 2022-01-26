d7d32d7af5b0cb41fb9796c1ced378d34bbc758f
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-01-26 19:53:40 +0100

    Un comment

--- src/components/home.js
+++ src/components/home.js
@@ -15,115 +15,113 @@ const radioButtonNames = {
   sentense: "Sentense",
 };
 
-//const playList = ["O6P86uwfdR0", "t2ypzz6gJm0", "5LrDIWkK_Bc"];
-
 function Home() {
-  // const [response, setResponse] = useState("");
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
-  // const [db, setDb] = useState(wordsListService);
-  // let [currentFetching, setCurrentFetching] = useState(radioButtonNames.words);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [db, setDb] = useState(wordsListService);
+  let [currentFetching, setCurrentFetching] = useState(radioButtonNames.words);
 
-  // useEffect(() => {
-  //   getAll();
-  // }, [currentFetching]);
+  useEffect(() => {
+    getAll();
+  }, [currentFetching]);
 
-  // function setFetch(event) {
-  //   currentFetching = event.target.name;
-  //   setCurrentFetching(currentFetching);
-  //   //console.log(event.target.name);
-  //   console.log(currentFetching);
-  // }
+  function setFetch(event) {
+    currentFetching = event.target.name;
+    setCurrentFetching(currentFetching);
+    //console.log(event.target.name);
+    console.log(currentFetching);
+  }
 
-  // function fetch() {
-  //   if (currentFetching == radioButtonNames.words) return db.fetchWords(); // returns a promise
-  //   if (currentFetching == radioButtonNames.phrasalVerb)
-  //     return db.fetchPhrasalVerb();
-  //   if (currentFetching == radioButtonNames.sentense) return db.fetchSentence();
-  // }
+  function fetch() {
+    if (currentFetching == radioButtonNames.words) return db.fetchWords(); // returns a promise
+    if (currentFetching == radioButtonNames.phrasalVerb)
+      return db.fetchPhrasalVerb();
+    if (currentFetching == radioButtonNames.sentense) return db.fetchSentence();
+  }
 
-  // function getAll() {
-  //   setLoading(true);
-  //   fetch()
-  //     .then(() => {
-  //       setDb({ ...db });
-  //       setLoading(false);
-  //     })
-  //     .catch((errorResponse) => {
-  //       setLoading(false);
-  //       setError(errorResponse);
-  //     });
-  // }
+  function getAll() {
+    setLoading(true);
+    fetch()
+      .then(() => {
+        setDb({ ...db });
+        setLoading(false);
+      })
+      .catch((errorResponse) => {
+        setLoading(false);
+        setError(errorResponse);
+      });
+  }
 
-  // function updateChange(event) {
-  //   setResponse(event.target.value);
-  // }
+  function updateChange(event) {
+    setResponse(event.target.value);
+  }
 
-  // function addWord(word) {
-  //   setLoading(true);
-  //   db.add(word).then(() => {
-  //     setDb({ ...db });
-  //     setLoading(false);
-  //   });
-  // }
-  // const radioButtons = (
-  //   <div className="btn-group" role="group" aria-label="Basic outlined example">
-  //     <button
-  //       type="button"
-  //       name={radioButtonNames.words}
-  //       onClick={setFetch}
-  //       className="btn btn-outline-primary"
-  //     >
-  //       {radioButtonNames.words}
-  //     </button>
-  //     <button
-  //       type="button"
-  //       name={radioButtonNames.phrasalVerb}
-  //       onClick={setFetch}
-  //       className="btn btn-outline-primary"
-  //     >
-  //       {radioButtonNames.phrasalVerb}
-  //     </button>
-  //     <button
-  //       type="button"
-  //       name={radioButtonNames.sentense}
-  //       onClick={setFetch}
-  //       className="btn btn-outline-primary"
-  //     >
-  //       {radioButtonNames.sentense}
-  //     </button>
-  //   </div>
-  // );
-  // const list =
-  //   currentFetching == radioButtonNames.sentense ? (
-  //     <SentenceList
-  //       setError={setError}
-  //       db={db}
-  //       wordsList={db.startsWith(response)}
-  //       setDb={setDb}
-  //       loading={loading}
-  //       setLoading={setLoading}
-  //     />
-  //   ) : (
-  //     <WordList
-  //       setError={setError}
-  //       db={db}
-  //       wordsList={db.startsWith(response)}
-  //       setDb={setDb}
-  //       loading={loading}
-  //       setLoading={setLoading}
-  //     />
-  //   );
+  function addWord(word) {
+    setLoading(true);
+    db.add(word).then(() => {
+      setDb({ ...db });
+      setLoading(false);
+    });
+  }
+  const radioButtons = (
+    <div className="btn-group" role="group" aria-label="Basic outlined example">
+      <button
+        type="button"
+        name={radioButtonNames.words}
+        onClick={setFetch}
+        className="btn btn-outline-primary"
+      >
+        {radioButtonNames.words}
+      </button>
+      <button
+        type="button"
+        name={radioButtonNames.phrasalVerb}
+        onClick={setFetch}
+        className="btn btn-outline-primary"
+      >
+        {radioButtonNames.phrasalVerb}
+      </button>
+      <button
+        type="button"
+        name={radioButtonNames.sentense}
+        onClick={setFetch}
+        className="btn btn-outline-primary"
+      >
+        {radioButtonNames.sentense}
+      </button>
+    </div>
+  );
+  const list =
+    currentFetching == radioButtonNames.sentense ? (
+      <SentenceList
+        setError={setError}
+        db={db}
+        wordsList={db.startsWith(response)}
+        setDb={setDb}
+        loading={loading}
+        setLoading={setLoading}
+      />
+    ) : (
+      <WordList
+        setError={setError}
+        db={db}
+        wordsList={db.startsWith(response)}
+        setDb={setDb}
+        loading={loading}
+        setLoading={setLoading}
+      />
+    );
 
   return (
     <div>
       <Navbar />
       <VideoPlayer videoUrl="" />
-      {/* <Search response={response} onChange={updateChange} />
+      <Search response={response} onChange={updateChange} />
       <AddWord name={response} add={addWord} />
       {error && <ErrorMessaje errorResponse={error} />}
       {radioButtons}
-      {list} */}
+      {list}
     </div>
   );
 }
